41dff9fc875f1951a706251113cc537fec5b1e31
OswaldoD <dreammicro7@hotmail.com>
2016-06-09 19:48:22 -0600

    problemas de permisos de archivos

--- src/analizadorSintactico/Main.java
+++ src/analizadorSintactico/Main.java
@@ -3,9 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package analizadorSintactico;
 
+import static analizadorSintactico.Main.SALIR;
 import java.io.File;
+import java.io.FileReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
@@ -15,7 +18,7 @@ import static java_cup.emit.parser;
 import java_cup.parser;
 
 /**
- * @author arturo
+ * @author esporras
  */
 public class Main {
 
@@ -29,25 +32,59 @@ public class Main {
      *
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){// throws Exception {
+        Visual(args);
+
+    }
+    
+    public static void Visual(String[] args){
         java.util.Scanner in = new Scanner(System.in);
         int valor = 0;
-        do {
+            System.out.println("********************************");
+            System.out.println("***** Compilador de Python *****");
+            System.out.println("********************************");            
             System.out.println("Elija una opcion: ");
             System.out.println("1) Generar Analizadores");
             System.out.println("2) Ejecutar Archivo");
             System.out.println("3) Salir");
             System.out.print("Opcion: ");
             valor = in.nextInt();
+            chooser(valor, args);
+    }
+    
+    public static void chooser(int valor, String[] args){
             switch (valor) {
                 /*  Generamos el analizador lexico y sintactico.
                  lcalc.flex contiene la definicion del analizador lexico
                  ycalc.cup contiene la definicion del analizador sintactico
                  */
                 case GENERAR: {
+                    generarAnalizadores(args);                    
+                    break;
+                }
+                case EJECUTAR: {
+                    ejecutarArchivo();
+                    Visual(args);
+                    break;
+                }
+                case SALIR: {
+                    System.out.println("Cerrando Programa!");
+                    break;
+                }
+                default: {
+                    System.out.println("Opcion no valida!");
+                    break;
+                }
+            }        
+    }
+    
+ 
+
+    
+    public static void  generarAnalizadores(String[] args){
                     System.out.println("\n*** Generando los analizadores ***\n");
                     String archLexico = "";
-                    String archSintactico = "";
+                    String archSintactico = "";     
                     if (args.length > 0) {
                         System.out.println("\n*** Procesando archivos custom ***\n");
                         archLexico = args[0];
@@ -55,8 +92,9 @@ public class Main {
                     } else {
                         System.out.println("\n*** Procesando archivo default ***\n");
                         archLexico = "Lexer.flex";
-                        archSintactico = "parser.cup";
+                        archSintactico = "parser.cup"; 
                     }
+                   
                     String[] alexico = {archLexico};
                     String[] asintactico = {"-parser", "AnalizadorSintactico", archSintactico};
                     jflex.Main.main(alexico);                    
@@ -68,54 +106,38 @@ public class Main {
                     //movemos los archivos generados
                     boolean mvAL = moverArch("AnalizadorLexico.java");
                     boolean mvAS = moverArch("AnalizadorSintactico.java");
-                    boolean mvSym= moverArch("sym.java");
-                    System.out.println("entré");
-                    System.out.println("Generado!");
-                    break;
-                }
-                case EJECUTAR: {
+                    boolean mvSym= moverArch("sym.java");                                        
+    }
+    
+    public static void ejecutarArchivo(){
                     /*  Ejecutamos el analizador lexico y sintactico
                      sobre un archivo de pruebas. 
                      */
-                    String[] archivoPrueba = {"entrada.txt"};
-               //   String[] archivoPrueba = {"entradaclase.txt"}; //archivo de clases
+                    String nombreArchivo="";
+                    
+                    System.out.println("Inserte el nombre del archivo: ");
+                    Scanner entradaConsola= new Scanner (System.in); //Creación de un objeto Scanner
+                    nombreArchivo= entradaConsola.nextLine (); //Invocamos un método sobre un objeto Scanner                                               
+                    String[] archivoPrueba = {nombreArchivo}; //archivo de clases
+                    AnalizadorSintactico.main(archivoPrueba);                                   
+                    System.out.println("\nArchivo Ejecutado\n");        
 
-                    AnalizadorSintactico.main(archivoPrueba);
-                    //AnalizadorSintactico p;
-                    //p = new AnalizadorSintactico((java_cup.runtime.Scanner) new AnalizadorSintactico((java_cup.runtime.Scanner) new java.io.FileInputStream("C://Users/esporras/Documents/NetBeansProjects/ProyectoCompi/src/archivosPrueba/for.txt")));
-                    //p.parse();
-                    System.out.println("Ejecutado!");
-                    break;
-                }
-                case SALIR: {
-                    System.out.println("Cerrando Programa!");
-                    break;
-                }
-                default: {
-                    System.out.println("Opcion no valida!");
-                    break;
-                }
             }
-        } while (valor != 3);
-
-    }
-
+    
     public static boolean moverArch(String archNombre) {
         boolean efectuado = false;
         File arch = new File(archNombre);
-        if (arch.exists()) {
-            System.out.println("\n*** Moviendo " + arch + " \n***");
+        if (arch.exists()) {            
             Path currentRelativePath = Paths.get("");
             String nuevoDir = currentRelativePath.toAbsolutePath().toString()
                     + File.separator + "src" + File.separator
                     + "analizadorSintactico" + File.separator + arch.getName();
             File archViejo = new File(nuevoDir);
             archViejo.delete();
-            if (arch.renameTo(new File(nuevoDir))) {
-                System.out.println("\n*** Generado " + archNombre + "***\n");
+            if (arch.renameTo(new File(nuevoDir))) {                
                 efectuado = true;
             } else {
-                System.out.println("\n*** No movido " + archNombre + " ***\n");
+                System.out.println("\n*** No se pueden generar los analizadores " + archNombre + " ***\n");
             }
 
         } else {
@@ -123,5 +145,6 @@ public class Main {
         }
         return efectuado;
     }
+    
+    
 }
-
